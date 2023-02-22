224e4554c08446e232db238ecc4f22e5979c7fc6
Leonardo Diaz <jldiaz9623@gmail.com>
2023-02-22 11:32:52 -0500

    Diseño login interfaz web

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,7 +33,6 @@ class Login extends Component {
         var respuesta=response[0]
         cookies.set('id', respuesta.id, {path: "/"})
         cookies.set('username', respuesta.username, {path: "/"})
-        alert(`Bienvenido ${respuesta.username}`)
         window.location.href="./Home"
       }else{
         alert("El usuario o contraseña no son correctos")
@@ -52,16 +51,58 @@ class Login extends Component {
 
   render(){
     return(
-      <div>
-        <label>Usuario: </label>
-        <br />
-        <input type="text" className='border-black-500 border-2' name="username" onChange={this.handleChange}/>
-        <br />
-        <label>Contraseña: </label>
-        <br />
-        <input type="password" className='border-black-500 border-2' name="password" onChange={this.handleChange}/>
-        <br />
-        <button className='border-black-500 border-2' onClick={()=> this.iniciarSesion()}>Iniciar Sesion</button>
+      <div className='grid grid-cols-1 absolute gap-0 w-full h-full'>
+        <div className='grid grid-cols-5 relative gap-0 '>
+          <div 
+            className="
+              md:col-span-3 
+              bg-gradient-to-r from-blue-500 to-green-500
+              bg-no-repeat
+              bg-cover
+              text-center
+              pt-48"
+          >
+            <h1 className='font-bold text-white text-8xl underline'>Bibliotek</h1>
+            <h3 className='text-white font-bold mt-4 text-2xl'>Nuestros libros, nuestro mundo</h3>
+          </div>
+          <div className='md:col-span-2 items-center justify-center bg-gray-800 back p-16'>
+            <div className='rounded-md bg-gradient-to-r from-blue-500 via-green-500 to-green-500 p-1 mt-24'>
+              <div className='items-center justify-center bg-gray-800 back p-8'>
+                <div className='mb-4'>
+                  <label class="block text-gray-100 text-sm font-bold mb-2" for="username">
+                    Nombre de Usuario
+                  </label>
+                  <input 
+                    type="text" 
+                    className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
+                    name="username" 
+                    id="username"
+                    placeholder='Nombre de Usuario'
+                    onChange={this.handleChange}/>
+                </div>
+                <div className='mb-4'>
+                  <label class="block text-gray-100 text-sm font-bold mb-2" for="password">Contraseña: </label>
+                  <input 
+                    type="password" 
+                    className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline' 
+                    name="password" 
+                    id="password"
+                    placeholder='******************'
+                    onChange={this.handleChange}/>
+                </div>
+                <div class="flex items-center justify-between">
+                  <button 
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' 
+                    onClick={()=> this.iniciarSesion()}
+                    type="button"
+                  >
+                    Iniciar Sesion
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
