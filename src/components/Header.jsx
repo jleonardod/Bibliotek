af52c48f4d7fa803c5cfa73236bd2e4d81d182be
Leonardo Diaz <jldiaz9623@gmail.com>
2023-03-09 14:23:07 -0500

    Esquema de usuario

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React, {useState } from "react"
-import { RiSearchLine, RiNotification2Line, RiMessage3Line } from "react-icons/ri"
+import { RiSearchLine, RiNotification2Line, RiMessage3Line, RiSettings5Line, RiUserSettingsLine } from "react-icons/ri"
 
 const Header = () => {
   const[showNotification, setShowNotification] = useState(false)
   const[showMensajes, setShowMensajes] = useState(false)
+  const[showUser, setShowUser] = useState(false)
 
   return(
     <header className="h-[7vh] md:h-[10vh] border-b border-primary p-2 flex justify-between align-items-center ">
@@ -13,19 +14,20 @@ const Header = () => {
       </div>
       <div className="">
         <div className="flex justify-between items-center h-full gap-4">
-          <button onClick={() => setShowNotification(!showNotification)} className="bg-white rounded-full p-2 cursor-pointer">
+          <button onClick={() => setShowNotification(!showNotification, setShowMensajes(false), setShowUser(false))} className="bg-white rounded-full p-2 cursor-pointer">
             <RiNotification2Line className="text-2xl" />
           </button>
-          <button onClick={() => setShowMensajes(!showMensajes)} className="bg-white rounded-full p-2 cursor-pointer">
+          <button onClick={() => setShowMensajes(!showMensajes, setShowNotification(false), setShowUser(false))} className="bg-white rounded-full p-2 cursor-pointer">
             <RiMessage3Line className="text-2xl" />
           </button>
           <img 
             src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
             className="object-cover w-full h-full rounded-full p-1 cursor-pointer"
+            onClick={() => setShowUser(!showUser, setShowNotification(false), setShowMensajes(false))}
           />
         </div>
       </div>
-      <div className={`${!showNotification ? "hidden" : "absolute" } right-36 top-14 border-gray-500 border-solid border-b border-l border-r border-t w-96`}>
+      <div className={`${!showNotification ? "hidden" : "absolute" } right-36 top-14 border-gray-500 border-solid border-b border-l border-r border-t w-96 z-20 bg-white`}>
         <h2 className="bg-primary w-full px-4 py-2 font-bold text-white text-2">Notificaciones</h2>
         <div className="ml-2">
           <ul className="py-2">
@@ -68,7 +70,7 @@ const Header = () => {
           </ul>
         </div>
       </div>
-      <div className={`${!showMensajes ? "hidden" : "absolute" } right-20 top-14 border-gray-500 border-solid border-b border-l border-r border-t w-96`}>
+      <div className={`${!showMensajes ? "hidden" : "absolute" } right-20 top-14 border-gray-500 border-solid border-b border-l border-r border-t w-96 z-10 bg-white`}>
         <h2 className="bg-primary w-full px-4 py-2 font-bold text-white text-2">Mensajes</h2>
         <div className="ml-2">
           <ul className="py-2">
@@ -105,6 +107,27 @@ const Header = () => {
                 <div>
                   <h2 className="font-bold text-xl">Contabilidad</h2>
                   <p className="text-gray-500">Mensaje 3</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={`${!showUser ? "hidden" : "absolute" } right-4 top-14 border-gray-500 border-solid border-b border-l border-r border-t w-56 z-10 bg-white`}>
+        <h2 className="bg-primary w-full px-4 py-2 font-bold text-white text-2">Jorge Leonardo Diaz</h2>
+        <div className="ml-2">
+          <ul className="py-2">
+            <li className="px-2 flex gap-4 mb-2">
+              <div className="pt-1">
+                <div className="flex flex-items justify-between items-center text-gray-700">
+                  <RiSettings5Line className="text-2xl mr-2" /> <span>Configuraciones</span> 
+                </div>
+              </div>
+            </li>
+            <li className="px-2 flex gap-4 mb-2">
+              <div className="pt-1">
+                <div className="flex flex-items justify-between items-center text-gray-700">
+                  <RiUserSettingsLine className="text-2xl mr-2" /> <span>Perfil</span> 
                 </div>
               </div>
             </li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,12 @@
-import React, {Component} from "react"
-import Cookies from 'universal-cookie'
+import React from "react"
 
-const cookies = new Cookies()
-
-class Home extends Component {
-  cerrarSesion = () => {
-    cookies.remove('id', {path:"/"})
-    cookies.remove('username', {path:"/"})
-
-    window.location.href="./"
-  }
-
-  componentDidMount(){
-    if(!cookies.get('id')){
-      window.location.href = './'
-    }
-  }
-
-  render(){
-    return(
-      <div>
-        Home
-
-        <br />
-
-        <button className='border-black-500 border-2' onClick={()=>this.cerrarSesion()}>Cerrar Sesión</button>
-      </div>
-    )
-  }
+const Home = () => {
+  return(
+    <div>
+      <h1 className="text-3xl font-bold">Dashboard</h1>
+    </div>
+  )
+  
 }
 
 export default Home
