9ee01b8cfca5af5282a2e1463b9864de06f7f059
Leonardo Diaz <jldiaz9623@gmail.com>
2023-03-09 11:50:32 -0500

    Esqueleto mensajes y Notificaciones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from "react"
+import React, {useState } from "react"
 import { RiSearchLine, RiNotification2Line, RiMessage3Line } from "react-icons/ri"
 
 const Header = () => {
+  const[showNotification, setShowNotification] = useState(false)
+  const[showMensajes, setShowMensajes] = useState(false)
+
   return(
     <header className="h-[7vh] md:h-[10vh] border-b border-primary p-2 flex justify-between align-items-center ">
       <div className="relative w-[30%] ml-4">
@@ -10,16 +13,102 @@ const Header = () => {
       </div>
       <div className="">
         <div className="flex justify-between items-center h-full gap-4">
-          <div className="bg-white rounded-full p-2">
+          <button onClick={() => setShowNotification(!showNotification)} className="bg-white rounded-full p-2 cursor-pointer">
             <RiNotification2Line className="text-2xl" />
-          </div>
-          <div className="bg-white rounded-full p-2">
+          </button>
+          <button onClick={() => setShowMensajes(!showMensajes)} className="bg-white rounded-full p-2 cursor-pointer">
             <RiMessage3Line className="text-2xl" />
-          </div>
+          </button>
           <img 
             src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
-            className="object-cover w-full h-full rounded-full p-1"
+            className="object-cover w-full h-full rounded-full p-1 cursor-pointer"
           />
+        </div>
+      </div>
+      <div className={`${!showNotification ? "hidden" : "absolute" } right-36 top-14 border-gray-500 border-solid border-b border-l border-r border-t w-96`}>
+        <h2 className="bg-primary w-full px-4 py-2 font-bold text-white text-2">Notificaciones</h2>
+        <div className="ml-2">
+          <ul className="py-2">
+            <li className="px-2 flex gap-4 mb-2">
+              <img 
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                className="object-cover w-16 h-16 rounded-full p-1 cursor-pointer"
+              />
+              <div className="pt-1">
+                <div>
+                  <h2 className="font-bold text-xl">Jefe</h2>
+                  <p className="text-gray-500">Notificación 1</p>
+                </div>
+              </div>
+            </li>
+            <li className="px-2 flex gap-4 mb-2">
+              <img 
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                className="object-cover w-16 h-16 rounded-full p-1 cursor-pointer"
+              />
+              <div className="pt-1">
+                <div>
+                  <h2 className="font-bold text-xl">Recursos Humanos</h2>
+                  <p className="text-gray-500">Notificación 2</p>
+                </div>
+              </div>
+            </li>
+            <li className="px-2 flex gap-4 mb-2">
+              <img 
+                src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                className="object-cover w-16 h-16 rounded-full p-1 cursor-pointer"
+              />
+              <div className="pt-1">
+                <div>
+                  <h2 className="font-bold text-xl">Contabilidad</h2>
+                  <p className="text-gray-500">Notificación 3</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={`${!showMensajes ? "hidden" : "absolute" } right-20 top-14 border-gray-500 border-solid border-b border-l border-r border-t w-96`}>
+        <h2 className="bg-primary w-full px-4 py-2 font-bold text-white text-2">Mensajes</h2>
+        <div className="ml-2">
+          <ul className="py-2">
+            <li className="px-2 flex gap-4 mb-2">
+              <img 
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                className="object-cover w-16 h-16 rounded-full p-1 cursor-pointer"
+              />
+              <div className="pt-1">
+                <div>
+                  <h2 className="font-bold text-xl">Jefe</h2>
+                  <p className="text-gray-500">Mensaje 1</p>
+                </div>
+              </div>
+            </li>
+            <li className="px-2 flex gap-4 mb-2">
+              <img 
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                className="object-cover w-16 h-16 rounded-full p-1 cursor-pointer"
+              />
+              <div className="pt-1">
+                <div>
+                  <h2 className="font-bold text-xl">Recursos Humanos</h2>
+                  <p className="text-gray-500">Mensaje 2</p>
+                </div>
+              </div>
+            </li>
+            <li className="px-2 flex gap-4 mb-2">
+              <img 
+                src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                className="object-cover w-16 h-16 rounded-full p-1 cursor-pointer"
+              />
+              <div className="pt-1">
+                <div>
+                  <h2 className="font-bold text-xl">Contabilidad</h2>
+                  <p className="text-gray-500">Mensaje 3</p>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
